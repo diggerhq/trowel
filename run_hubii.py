c78fcbe3dea723bd09bf6139bc636ddce02b6eb1
alexey <alexey@digger.dev>
2023-01-05 12:29:30 +0000

    add hubii config

---
+++ run_hubii.py
@@ -0,0 +1,31 @@
+import configparser
+import json
+import os
+from distutils.dir_util import copy_tree
+from pathlib import Path
+from utils import generate_terraform_project
+
+config = configparser.ConfigParser()
+# without this line all keys will be in lower case
+config.optionxform = str
+
+current_dir = os.getcwd()
+
+with open('./test_configs/hubii_minimal.json', 'r') as f:
+    bundle_spec = json.loads(f.read())
+config.read('./test_configs/hubii.ini')
+
+for k, v in config['secrets'].items():
+    bundle_spec['secret_keys'].append(str(k))
+
+for m in bundle_spec['blocks']:
+    if m['type'] == 'container':
+        for k, v in config['envs'].items():
+            m['environment_variables'].append({'key': k, 'value': v})
+        #for k, v in config['secrets'].items():
+        #    m['secrets'].append(str(k))
+
+home_path = str(Path.home())
+generate_terraform_project(f"{home_path}/tmp/hubii", bundle_spec)
+copy_tree(f"{home_path}/tmp/hubii/terraform", f"{home_path}/tmp/test-hubii")
+
