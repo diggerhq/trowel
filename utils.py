ce36642e3278164e71137dba432763ca438aaeb3
Dias Saparov <dias@digger.dev>
2023-01-06 17:43:37 +0000

    add imported validation

--- utils.py
+++ utils.py
@@ -541,7 +541,8 @@ def generate_terraform_project(terraform_project_dir, config):
 
             if "secret_keys" in m:
                 block_secrets[m['name']] = m['secret_keys']
-
+        if m["type"] == "imported":
+            process_custom_terraform(dest_dir=terraform_dir, custom_terraform=m['custom_terraform'])
     ecs_security_groups = f'[{",".join(ecs_security_groups_list)}]'
     print(f"ecs_security_groups: {ecs_security_groups}")
     for m in config["blocks"]:
