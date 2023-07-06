3e6dd107b37d5b10a2212e4859f2b45f524bd59e
alexey <alexey@digger.dev>
2023-07-06 11:17:26 +0100

    fix validation

--- validators.py
+++ validators.py
@@ -1,8 +1,8 @@
-from distutils.util import strtobool
 
 def validate_bastion_parameters(config: dict):
     if "enable_bastion" in config:
-        enable_bastion = strtobool(config["enable_bastion"])
+        if not isinstance(config["enable_bastion"], bool):
+            raise ValueError("enable_bastion is not boolean")
     if "bastion_ssh_key_name" not in config:
         raise ValueError("bastion_ssh_key_name is mandatory parameter if bastion is enabled")
     if "bastion_instance_name" not in config:
